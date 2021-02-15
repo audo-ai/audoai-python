a83afa39a8a16ca50777ef93cb83ae934a40bfba
Matthew D. Scholefield <matthew331199@gmail.com>
2021-02-14 22:12:08 -0600

    Increment audoai-common to 0.1.1

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='audoai-common',
-    version='0.1.0',
+    version='0.1.1',
     description='Library containing shared code for Audo AI APIs',
     url='https://github.com/audo-ai/audoai-python',
     author='Audo AI',
